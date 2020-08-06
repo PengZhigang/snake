b43f05cea0b0dfb7e6fa7aea50cca5ee5d40f14c
Peng <pzg1994@gmail.com>
2020-08-06 19:12:08 +0300

    supp

--- snake.py
+++ snake.py
@@ -1,184 +1,372 @@
 
-# -*- coding: utf-8 -*-
-import sys,pygame,random
-from pygame.locals import *
+from Tkinter import *
+import random
+from time import *
+import thread
+import sys
+sys.setrecursionlimit(sys.maxint)
 
-windowWidth=600
-windowHeight=600
-cellSize=20
-head = 0
+def callback1():  # difficulty
+    global diff
+    diff = difficulty(0)
 
-def main():
-    #初始化
-    pygame.init()
-    global screen
-    screen=pygame.display.set_mode((windowWidth,windowHeight))
-    pygame.display.set_caption("Greedy Snake")
-    gameStart()
-    FPS=pygame.time.Clock()
-    startX=cellSize*random.randint(8,windowWidth/cellSize-8)
-    startY=cellSize*random.randint(8,windowHeight/cellSize-8)
+def callback11():  # difficulty
+    global diff
+    diff = difficulty(1)
 
-    snake=[{'x': startX , 'y': startY},
-           {'x': startX - 1 * cellSize, 'y': startY},
-            {'x': startX - 2 * cellSize, 'y': startY},
-           {'x': startX - 3 * cellSize, 'y': startY}]
-    appleX = cellSize * random.randint(0, windowWidth / cellSize)
-    appleY = cellSize * random.randint(0, windowHeight / cellSize)
+def callback12():  # difficulty
+    global diff
+    diff = difficulty(2)
 
-    while True:
-        FPS.tick(15)
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type==KEYDOWN:
-                turn(snake,event)
-        move(snake)
-        if snake[head]['x']==appleX and snake[head]['y']==appleY:
-            appleX = cellSize * random.randint(0, windowWidth / cellSize)
-            appleY = cellSize * random.randint(0, windowHeight / cellSize)
-            eat(snake)
+def callback2():  # start
+    start()
 
-        if snake[head]['x']<0 or snake[head]['x']>windowWidth or snake[head]['y']<0 or snake[head]['y']>windowHeight:
-            gameOver(snake)
-        for body in snake[1:]:
-            if body==snake[head]:
-                gameOver(snake)
+def callback3():  # exit
+    root.destroy()
 
-        screen.fill((0, 150, 0))
-        drawgrid()
-        drawSnake(snake)
-        pygame.draw.rect(screen, (255, 0, 0), (appleX, appleY, cellSize, cellSize))
+def callback4(event):  # up
+    m.append(0)
 
-        pygame.display.update()
+def callback5(event):  # down
+    m.append(1)
 
-#绘制网格
-def drawgrid():
-    for i in xrange(cellSize,windowWidth,cellSize):
-        pygame.draw.line(screen,(0,255,0),(i,0),(i,windowHeight),1)
-    for j in xrange(cellSize,windowHeight,cellSize):
-        pygame.draw.line(screen,(0,255,0),(0,j),(windowWidth,j),1)
+def callback6(event):  # left
+    m.append(2)
 
-#绘制蛇
-def drawSnake(snake):
-    for cood in snake:
-        pygame.draw.rect(screen,(150,150,0),(cood['x'],cood['y'],cellSize,cellSize))
-    pygame.draw.rect(screen, (250, 250, 0), (snake[head]['x'], snake[head]['y'], cellSize, cellSize))
+def callback7(event):  # right
+    m.append(3)
 
-def turn(snake,event):
-    # 上下转向
-    if snake[head]['y'] == snake[1]['y']:
-        if event.key == K_UP:
-            del snake[-1]
-            newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] - cellSize}
-            snake.insert(0, newHead)
+def difficulty(x=2):
+    if x == 0:
+        return 0.5
+    if x == 1:
+        return 0.3
+    if x == 2:
+        return 0.1
 
-        elif event.key == K_DOWN:
-            del snake[-1]
-            newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] + cellSize}
-            snake.insert(0, newHead)
+def createfood():
+    x = random.randint(0, 39)
+    y = random.randint(0, 29)
+    return (x, y)
 
-    # 左右转向
-    if snake[head]['x'] == snake[1]['x']:
-        if event.key == K_LEFT:
-            del snake[-1]
-            newHead = {'x': snake[head]['x'] - cellSize, 'y': snake[head]['y']}
-            snake.insert(0, newHead)
+def pan(now):
+    for i in range(len(l)):
+        if lx[i] == now[0] * 20 and ly[i] == now[1] * 20:
+            return False
 
-        if event.key == K_RIGHT:
-            del snake[-1]
-            newHead = {'x': snake[head]['x'] + cellSize, 'y': snake[head]['y']}
-            snake.insert(0, newHead)
+def food():
+    now = createfood()
+    while pan(now):
+        now = createfood()
+    r = f.create_rectangle(now[0] * 20, now[1] * 20, now[0] * 20 + 20, now[1] * 20 + 20, fill='red')
+    f.update()
+    z.append(r)
+    lxf.append(now[0] * 20)
+    lyf.append(now[1] * 20)
 
+def gameover():
+    if lx[-1] > 780 or lx[-1] < 0 or ly[-1] > 580 or ly[-1] < 0:
+        try:
+            f.create_text(400, 250, text='Game Over', font=('Fixdsys', 40), fill='red', justify=CENTER)
+            f.create_text(400, 330, text='Ur Score : ' + str(len(l) - 3), font=('Fixdsys', 40), fill='blue', justify=CENTER)
+            f.create_text(400, 400, text='please click exit', fill='black',font=('Fixdsys', 20))
+        except:
+            print('Game Over')
+            f.create_text(400,400,text='please click exit',fill='black',font=('Fixdsys', 20))
+            sleep(100)
+    for i in range(len(l) - 1):
+        if lx[i] == lx[-1] and ly[i] == ly[-1]:
+            try:
+                f.create_text(400, 250, text='Game Over', font=('Fixdsys', 40), fill='red', justify=CENTER)
+                f.create_text(400, 330, text='Ur Score : ' + str(len(l) - 3), font=('Fixdsys', 40), fill='blue',justify=CENTER)
+                f.create_text(400, 400, text='please click exit', fill='black',font=('Fixdsys', 20))
+            except:
+                print('Game Over')
+                f.create_text(400, 400, text='please click exit', fill='black',font=('Fixdsys', 20))
+                sleep(100)
 
-def move(snake):
-    # 向左运动
-    if snake[head]['x'] < snake[1]['x']:
-        del snake[-1]
-        newHead = {'x': snake[head]['x'] - cellSize, 'y': snake[head]['y']}
-        snake.insert(0, newHead)
-    # 向右运动
-    elif snake[head]['x'] > snake[1]['x']:
-        del snake[-1]
-        newHead = {'x': snake[head]['x'] + cellSize, 'y': snake[head]['y']}
-        snake.insert(0, newHead)
-    # 向上运动
-    elif snake[head]['y'] < snake[1]['y']:
-        del snake[-1]
-        newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] - cellSize}
-        snake.insert(0, newHead)
-    # 向下运动
-    elif snake[head]['y'] > snake[1]['y']:
-        del snake[-1]
-        newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] + cellSize}
-        snake.insert(0, newHead)
+#控制移动函数
+def move():
+    if m[-1] == 0:
+        up_move1()
+        m.pop(0)
+    elif m[-1] == 1:
+        down_move1()
+        m.pop(0)
+    elif m[-1] == 2:
+        left_move1()
+        m.pop(0)
+    elif m[-1] == 3:
+        right_move1()
+        m.pop(0)
 
-def eat(snake):
-    # 向左运动
-    if snake[head]['x'] < snake[1]['x']:
-        newHead = {'x': snake[head]['x'] - cellSize, 'y': snake[head]['y']}
-        snake.insert(0, newHead)
-    # 向右运动
-    elif snake[head]['x'] > snake[1]['x']:
-        newHead = {'x': snake[head]['x'] + cellSize, 'y': snake[head]['y']}
-        snake.insert(0, newHead)
-    # 向上运动
-    elif snake[head]['y'] < snake[1]['y']:
-        newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] - cellSize}
-        snake.insert(0, newHead)
-    # 向下运动
-    elif snake[head]['y'] > snake[1]['y']:
-        newHead = {'x': snake[head]['x'], 'y': snake[head]['y'] + cellSize}
-        snake.insert(0, newHead)
+def crash():
+    for i in range(len(l) - 1):
+        if lx[i] == lx[-1] and ly[i] == ly[-1]:
+            return True
 
-def gameStart():
-    screen.fill((0, 150, 0))
-    drawgrid()
-    fontObj1 = pygame.font.Font('freesansbold.ttf', 60)
-    textSurfaceObj1 = fontObj1.render('Greedy Snake', True, (255,255,255))
-    textRectObj1 = textSurfaceObj1.get_rect()
-    textRectObj1.center = (windowWidth/2, windowHeight/3)
-    screen.blit(textSurfaceObj1,textRectObj1)
+#自动移动
+def up_move():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1], ly[-1] - 20, lx[-1] + 20, ly[-1], fill='white')
+        l.append(r)
+        lx.append(lx[-1])
+        ly.append(ly[-1] - 20)
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+        if len(m) != 0:
+            thread.start_new_thread(move, ())
+        sleep(diff)
+        move_straight()
 
-    fontObj2 =pygame.font.Font('freesansbold.ttf', 30)
-    textSurfaceObj2 = fontObj2.render('Devloper: JIE ', True,(0, 0, 0))
-    textRectObj2 = textSurfaceObj2.get_rect()
-    textRectObj2.center = (windowWidth/2, windowHeight*2/3)
-    screen.blit(textSurfaceObj2,textRectObj2)
+#自动移动
+def down_move():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1], ly[-1] + 20, lx[-1] + 20, ly[-1] + 40, fill='white')
+        f.update()
+        l.append(r)
+        lx.append(lx[-1])
+        ly.append(ly[-1] + 20)
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+        if len(m) != 0:
+            thread.start_new_thread(move, ())
+        sleep(diff)
+        move_straight()
 
-    pygame.display.update()
-    flag=True
-    while(flag):
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
-            if event.type==KEYDOWN:
-                if event.key==K_SPACE:
-                    flag=False
+#自动移动
+def left_move():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1] - 20, ly[-1], lx[-1], ly[-1] + 20, fill='white')
+        f.update()
+        l.append(r)
+        lx.append(lx[-1] - 20)
+        ly.append(ly[-1])
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+        if len(m) != 0:
+            thread.start_new_thread(move, ())
+        sleep(diff)
+        move_straight()
 
+#自动移动
+def right_move():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1] + 20, ly[-1], lx[-1] + 40, ly[-1] + 20, fill='white')
+        l.append(r)
+        lx.append(lx[-1] + 20)
+        ly.append(ly[-1])
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            f.update()
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+        if len(m) != 0:
+            thread.start_new_thread(move, ())
+        sleep(diff)
+        move_straight()
 
-def gameOver(snake):
-    screen.fill((0, 150, 0))
-    drawgrid()
-    fontObj = pygame.font.Font('freesansbold.ttf', 60)
-    textSurfaceObj1 = fontObj.render('Game over!', True, (255,0,0))
-    textRectObj1 = textSurfaceObj1.get_rect()
-    textRectObj1.center = (windowWidth/3, windowHeight/3)
-    screen.blit(textSurfaceObj1,textRectObj1)
+#自动移动
+def move_straight():  # 坐标比较 然后调用四个方向移动的函数
+    if lx[-1] == lx[-2] and ly[-1] > ly[-2]:
+        down_move()
+    if lx[-1] == lx[-2] and ly[-1] < ly[-2]:
+        up_move()
+    if lx[-1] > lx[-2] and ly[-1] == ly[-2]:
+        right_move()
+    if lx[-1] < lx[-2] and ly[-1] == ly[-2]:
+        left_move()
 
-    textSurfaceObj2 = fontObj.render('Score: %s' % (len(snake)-4), True, (255, 0, 0))
-    textRectObj2 = textSurfaceObj2.get_rect()
-    textRectObj2.center = (windowWidth*2/3, windowHeight*2/3)
-    screen.blit(textSurfaceObj2,textRectObj2)
+#控制移动函数
+def up_move1():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    r = f.create_rectangle(lx[-1], ly[-1] - 20, lx[-1] + 20, ly[-1], fill='white')
+    l.append(r)
+    lx.append(lx[-1])
+    ly.append(ly[-1] - 20)
+    gameover()
+    if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+        f.delete(l[0])
+        l.pop(0)
+        lx.pop(0)
+        ly.pop(0)
+    else:  # 碰到了
+        f.delete(z[-1])
+        food()
+    f.update()
 
-    pygame.display.update()
-    while(True):
-        for event in pygame.event.get():
-            if event.type==QUIT:
-                pygame.quit()
-                sys.exit()
+#控制移动函数
+def down_move1():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1], ly[-1] + 20, lx[-1] + 20, ly[-1] + 40, fill='white')
+        f.update()
+        l.append(r)
+        lx.append(lx[-1])
+        ly.append(ly[-1] + 20)
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
 
-if __name__ == '__main__':
-    main()
+#控制移动函数
+def left_move1():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1] - 20, ly[-1], lx[-1], ly[-1] + 20, fill='white')
+        f.update()
+        l.append(r)
+        lx.append(lx[-1] - 20)
+        ly.append(ly[-1])
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+
+#控制移动函数
+def right_move1():
+    if lx[-1] < 0 or lx[-1]>780 or ly[-1]<0 or ly[-1]>580:
+        pass
+    elif crash():
+        pass
+    else:
+        r = f.create_rectangle(lx[-1] + 20, ly[-1], lx[-1] + 20 + 20, ly[-1] + 20, fill='white')
+        l.append(r)
+        lx.append(lx[-1] + 20)
+        ly.append(ly[-1])
+        gameover()
+        if lx[-1] != lxf[-1] or ly[-1] != lyf[-1]:  # 没碰到
+            f.delete(l[0])
+            f.update()
+            l.pop(0)
+            lx.pop(0)
+            ly.pop(0)
+        else:  # 碰到了
+            f.delete(z[-1])
+            food()
+        f.update()
+
+def start():
+    global r1
+    global r2
+    l.append(r1)
+    l.append(r2)
+    lx.append(0)
+    lx.append(20)
+    ly.append(0)
+    ly.append(0)
+    food()
+    move_straight()
+
+root = Tk()
+root.title('snake')
+root.geometry('800x600')
+m = Menu(root)
+root.config(menu=m)
+m.add_command(label='Start', command=callback2)
+difmenu = Menu(m)
+m.add_cascade(label='Difficulty', menu=difmenu)
+difmenu.add_command(label='Easy', command=callback1)
+difmenu.add_separator()
+difmenu.add_command(label='Normal', command=callback11)
+difmenu.add_separator()
+difmenu.add_command(label='Difficult', command=callback12)
+paumenu = Menu(m)
+m.add_cascade(label='Pause', menu=paumenu)
+m.add_command(label='Exit', command=callback3)
+f = Canvas(root, width=800, height=600, bg='white')
+f.bind('<Up>', callback4)
+f.bind('<Down>', callback5)
+f.bind('<Left>', callback6)
+f.bind('<Right>', callback7)
+f.focus_set()
+f.pack()
+
+#这里需要一张底色不为白色、gif格式的图片，可以自行调整（我划的线是白色）
+#pic = PhotoImage(file='C:\Users\wj\Desktop\\snake.gif')
+#f.create_image(400, 300, image=pic)
+
+for i in range(20 - 1, 600 - 1, 20):
+    f.create_line(0, i, 800, i, smooth=0, fill='white')
+
+for i in range(20 - 1, 800 - 1, 20):
+    f.create_line(i, 0, i, 600, smooth=0, fill='white')
+
+l = []
+lx = []
+ly = []
+lxf = []
+lyf = []
+z = []
+m = []
+r1 = f.create_rectangle(0, 0, 20, 20, fill='white')
+r2 = f.create_rectangle(20, 0, 40, 20, fill='white')
+diff = difficulty()
+root.mainloop()
